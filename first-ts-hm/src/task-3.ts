6a22f3327913226e30336ba9640d0aa75b9f5b36
Andrew Kruhluk <kruglikandrej9@gmail.com>
2025-06-23 12:23:51 +0300

    hm

---
+++ first-ts-hm/src/task-3.ts
@@ -0,0 +1,16 @@
+const usernames: Array<string> = ["alice", "bob", "charlie"];
+const ratings: number[] = [4.5, 3.8, 5];
+
+
+interface Product{
+    readonly id: number,
+    title: string,
+}
+
+
+const products: Product[] = [
+    { id: 1, title: "Phone" },
+    { id: 2, title: "Laptop" },
+
+]
+
